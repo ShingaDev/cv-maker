a88a1199f16a623e6d0d1fa516e2331fb1499001
Shinga <emre.artdesign@gmail.com>
2023-03-22 00:30:15 +0300

    build: Add new component
    New create header component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
-import Form from "./Form";
+import Header from "./components/Header";
 
 function App() {
   return (
     <div className="flex flex-col items-center justify-center h-screen">
-      <Form />
+      <Header />
     </div>
   );
 }
